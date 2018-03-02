8b6ad6a61b4f4e302bd427b75ec86ed62216d0ab
Angelo Manganiello <angelo.mang@libero.it>
2018-03-02 10:44:40 +0100

    gradle mongodb web service starter project

--- frontend-build-install.js
+++
@@ -1,9 +0,0 @@
-const args = process.argv;
-var exec = require('child_process').execSync;
-var cmd = "cd frontend-app && "+ args[2]+" " + args[3] +" "+args[4] ;
-
-var options = {
-  encoding: 'utf8'
-};
-
-console.log(exec(cmd, options));
